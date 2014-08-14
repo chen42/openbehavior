b6ac30961cf40c55f55d43ad1bf2cf4c93c7e5d5
Wes <root@raspberrypi.(none)>
2014-08-14 17:48:55 +0000

    fixed mounting issues

--- infrared/gpio.ir.py
+++ infrared/gpio.ir.py
@@ -10,11 +10,8 @@ import pwd
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-interval = 200 # 20 milli second 
-threshould = 50 # minimal counts in time interval
 pinCount = input("Enter number of pins in use by IR sensors: ")
 channels = []
-cnt, mili, diff = ({} for i in range(3))
 pin_alloc = 0
 
 #get pins in use
@@ -28,28 +25,23 @@ while pin_alloc < pinCount:
 		 
 #initialize counts
 for c in channels:
-	cnt[c] = 0
-	mili[c] = (int(round(time.time() * 1000)))
-	diff[c] = 0
 	print str(c)
 	GPIO.setup(c, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-fo = open("ir_log.txt", "a")
+fo = open("/home/pi/data/ir_log.txt", "a")
 uid = pwd.getpwnam("pi").pw_uid
 gid = grp.getgrnam("pi").gr_gid
-path = "ir_log.txt"
+path = "/home/pi/data/ir_log.txt"
 os.chown(path, uid, gid)
 
 def intervalCounting(channel): 
-	fo = open("ir_log.txt", "a")
-	cnt[channel] += 1
-	currentTime = int(round(time.time()*1000))
+	fo = open("/home/pi/data/ir_log.txt", "a")
 	if GPIO.input(channel):
 		currentTime = int(round(time.time()*1000))
 		fo.write(str(currentTime) + "\tchannel " + str(channel) + "\tblocked\n")
 	else:
 		currentTime = int(round(time.time()*1000))
-		fo.write(str(currentTime) + "\tchannel " + str(channel) + "\topen\b")
+		fo.write(str(currentTime) + "\tchannel " + str(channel) + "\topen\n")
 	return
 
 for c in channels:
