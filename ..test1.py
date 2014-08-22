d938d78516c6e9134e65f614fbfbc868c6850dfa
Ashlesh Gawande <ashu2493@gmail.com>
2014-08-21 20:25:11 -0500

    Fixing input issue, data formating. Adding initial tests with scratch

---
+++ ..test1.py
@@ -0,0 +1,19 @@
+import scratch
+s = scratch.Scratch()
+
+# to make a broadcast to scratch
+s.broadcast("start")
+
+# to receive an update from scratch
+message = s.receive()
+# blocks until an update is received
+# message returned as  {'broadcast': [], 'sensor-update': {'scratchvar': '64'}}
+#                  or  {'broadcast': ['from scratch'], 'sensor-update': {}}
+# where scratchvar is the name of a variable in scratch
+# and 'from scratch' is the name of a scratch broadcast
+
+# send sensor updates to scratch
+data = {}
+data['pyvar'] = 123
+for data['pycounter'] in range(60):
+    s.sensorupdate(data)
