d88671654c8d51e05baf0082c2bf5821deba9e29
Hao Chen <inbox@haochen.name>
2015-02-08 22:41:02 -0600

    putting I2C env sensors together

---
+++ envSensors/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## NOTE: pigpoid can be found here: wget abyz.co.uk/rpi/pigpio/pigpio.zip
+## need to install the following:
+## enable I2C: https://learn.adafruit.com/adafruits-raspberry-pi-lesson-4-gpio-setup/configuring-i2c
+## install Adafruit library for the MCP9808 https://learn.adafruit.com/mcp9808-temperature-sensor-python-library/software
+
+## The following commands need to be run before the env log file
+
+sudo chmod 666   /sys/module/i2c_bcm2708/parameters/combined
+sudo echo -n 1 > /sys/module/i2c_bcm2708/parameters/combined
+sudo pigpiod
+
+
+
