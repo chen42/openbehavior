ed769034c2767d7f9c32e73e7b166cb902679135
Hao Chen <inbox@haochen.name>
2015-07-24 13:25:52 -0500

    updated flash light when lux is low

--- envSensors/checklux.py
+++ envSensors/checklux.py
@@ -11,12 +11,12 @@ GPIO.setup(11, GPIO.OUT)
 
 sum = 0
 count = 0
+threshold = 5
 with open('/home/pi/lux.csv') as f:
 	for line in f:
-		if float(line) <= 68:
-			sum += float(line)
-			count = count + 1
+		sum += float(line)
+		count = count + 1
 	avg = sum/count			
 	print avg
-	while avg <= 68:
+	while avg <= threshold:
 		blink(11)
