c4b9c6bd80eee4e01b6cd92bf861e498b445df58
Hao Chen <inbox@haochen.name>
2015-07-28 10:16:18 -0500

    put pin number in a variable

--- envSensors/checklux.py
+++ envSensors/checklux.py
@@ -1,13 +1,19 @@
 import RPi.GPIO as GPIO
 import time
+
+
+pin=11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
+
+
 def blink(pin):
 	GPIO.output(pin,GPIO.HIGH)
 	time.sleep(.2)
 	GPIO.output(pin,GPIO.LOW)
 	time.sleep(.2)
 	return
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+
 
 sum = 0
 count = 0
@@ -19,4 +25,4 @@ with open('/home/pi/lux.csv') as f:
 	avg = sum/count			
 	print avg
 	while avg <= threshold:
-		blink(11)
+		blink(pin)
