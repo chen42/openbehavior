9bccd2b0046fcd95749cada7191d871c38a586c2
Matthew Longley <randomticktock@gmail.com>
2016-01-14 12:12:59 -0600

    Added LCD test code

---
+++ RFID/lcd_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import Adafruit_CharLCD
+
+# create new lcd object
+lcd = Adafruit_CharLCD()
+
+# Display some stuff
+lcd.clear()
+lcd.message("Scientia est\n")
+lcd.message("\tluxlucis")
