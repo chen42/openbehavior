f69488a5599c4f3d2deafc13c1196add4dd888cd
Matthew Longley <randomticktock@gmail.com>
2016-01-21 10:57:03 -0600

    Cleaned up RFID directory and moved things around

--- RFID/rfidscanner.py
+++ RFID/rfidscanner.py
@@ -27,7 +27,6 @@ def main():
 	# UART data
 	idstring = ""
 	chardata = 0
-	checksum = 0
 	
 	# initialize the LCD
 	lcd = Adafruit_CharLCD.Adafruit_CharLCD()
@@ -50,7 +49,6 @@ def main():
 	while True:
 		time.sleep(0.25)
 		# zero out the variables
-		#checksum = 0
 		tag = 0
 		idstring = ""
 		# read in a character
@@ -61,15 +59,6 @@ def main():
 			for i in range(12):
 				chardata = UART.read()
 				idstring = idstring + str(chardata)
-			# remove end flag from the string
-			#idstring = idstring.replace(endflag, "")
-			# calculate checksum
-			#for i in range(0, 9, 2):
-			#	checksum = checksum ^ (((int(idstring[i], 16)) << 4) + int(idstring[i+1], 16))
-			#checksum = hex(checksum)
-			# filter tag
-			#tag = ((int(idstring[1], 16) << 8) + ((int(idstring[2], 16)) << 4) + ((int(idstring[3], 16)) << 0)
-			# tag = hex(tag)
 			printIDtoLCD(lcd, idstring)
 			with open(datafile, "a") as f:
 				f.write(time.strftime("%Y-%m-%d\t%H:%M:%S\t") + idstring + "\n")
