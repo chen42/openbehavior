13d2fba3aefc506d5629813f616b1209f752cc0f
Matthew Longley <randomticktock@gmail.com>
2016-02-05 10:51:12 -0600

    Translated pin numbers to wiring spec and put them in header macros

--- pump-control/pumpcontrol.h
+++ pump-control/pumpcontrol.h
@@ -1,4 +1,14 @@
 #ifndef PUMPCONTROL_H
 #define PUMPCONTROL_H
 
+/* Macro definitions */
+
+#define DIR_PIN 0
+#define STEP_PIN 2
+#define SLEEP_PIN 3
+#define MS3_PIN 12
+#define MS2_PIN 13
+#define MS1_PIN 14
+#define SW1_PIN 25
+
 #endif /* PUMPCONTROL_H */
