3366defef8736a1b08da24cc2c6b0a12450892be
Matthew Longley <randomticktock@gmail.com>
2016-02-29 10:06:51 -0600

    forgot to make the main program actually fork

--- pump-control/python/main.py
+++ pump-control/python/main.py
@@ -91,9 +91,9 @@ dlogger = datalogger.DataLogger()
 
 while True:
 	if gpio.input(SW1):
-		pump.move(1)
+		forkPump(1)
 	elif gpio.input(SW2):
-		pump.move(-1)
+		forkPump(-1)
 	elif not gpio.input(TIR):
 		i = tsensor.readPinTouched()
 		if i == 1:
@@ -105,7 +105,7 @@ while True:
 					pumptimedout = True
 					pumpTimer = Timer(timeout, resetPumpTimeout)
 					pumpTimer.start()
-					pump.move(-1)
+					forkPump(1)
 				else:
 					dlogger.logTouch("ACTIVE")
 			else:
