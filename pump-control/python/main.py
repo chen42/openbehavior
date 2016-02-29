8ec72be589b9007e4a20268e91f0c8f94dddab88
Matthew Longley <randomticktock@gmail.com>
2016-02-29 10:48:38 -0600

    Revert "welp everything is fucked"
    This reverts commit e4525955e93c381217269a9e296cee3f3064023b.

--- pump-control/python/main.py
+++ pump-control/python/main.py
@@ -53,8 +53,9 @@ def forkPump(movement):
 	pumpArg = str(movement)
 	retFork = os.fork()
 	if retFork == 0:
-		os.system("/home/pi/openbehavior/pump-control/python/pumpcontrol.py -m " + pumpArg)
-		sys.exit(0)
+		args = ("-m", pumpArg)
+		os.execvp("/home/pi/openbehavior/pump-control/python/pumpcontrol.py", args)
+		return
 	else:
 		return
 
