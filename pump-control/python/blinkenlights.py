534ed7ab90dc397c81640226a9003825e2185db5
Matthew Longley <randomticktock@gmail.com>
2016-03-03 13:31:05 -0600

    der blinkenlights

--- pump-control/python/blinkenlights.py
+++ pump-control/python/blinkenlights.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 
 import RPi.GPIO as gpio
+import time
 
-def blinkenlights(duration):
+# BEGIN CONSTANT DEFINITIONS
+GREEN_LED_PIN = int(28)
+# END CONSTANT DEFINITIONS
+
+def flashRewardLED(duration):
+	gpio.output(GREEN_LED_PIN, gpio.HIGH)
+	time.sleep(duration)
+	gpio.output(GREEN_LED_PIN, gpio.LOW)
 	
+if __name__ == "__main__":
+	# Initialize GPIO
+	gpio.setwarnings(False)
+	gpio.setmode(gpio.BOARD)
+	gpio.setup(GREEN_LED_PIN, gpio.OUT)
