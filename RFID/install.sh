ecd1371b0cef04c652d642a69d9c5778c4f42ba5
Hao Chen <inbox@haochen.name>
2016-04-26 14:36:01 -0500

    add date to the LCD message

--- RFID/install.sh
+++ RFID/install.sh
@@ -3,15 +3,16 @@
 # Clone the openbehavior repository to the home directory of user 'pi' and
 # then run this script with root permissions to install RFID scanner
 
-if [ ! -d "/home/pi/Sync" ]; then
-	mkdir "/home/pi/Sync"
-	chown pi:users "/home/pi/Sync"
+if [ ! -d "/home/pi/Pies" ]; then
+	mkdir "/home/pi/Pies"
+	chown pi:users "/home/pi/Pies"
 fi
 
-if [ ! -e "/home/pi/Sync/rfidreader.log" ]; then
-	touch "/home/pi/Sync/rfidreader.log"
-	chown pi:users "/home/pi/Sync/rfidreader.log"
+if [ ! -d "/home/pi/Pies/RFIDReader" ]; then
+	mkdir "/home/pi/Pies/RFIDReader"
+	chown pi:users "/home/pi/Pies/RFIDReader"
 fi
 
 cp /home/pi/openbehavior/RFID/rfidscanner.service /etc/systemd/system/rfidscanner.service
 systemctl enable rfidscanner.service
+
