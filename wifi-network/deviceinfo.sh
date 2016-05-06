e0593df815f8409b0576c2775c174b1cdebc02dd
Hao Chen <inbox@haochen.name>
2016-05-06 12:49:14 -0500

    adjust system clock so it will work with htpdate

--- wifi-network/deviceinfo.sh
+++ wifi-network/deviceinfo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+year=`date |cut -d " " -f 7 ` 
+if [ "$year" -lt "2016" ] 
+	then
+		date -s "Fri May 6 1:01:01 CDT 2016"
+		htpdate -s www.freebsd.org www.linux.org www.google.com	
+		hwclock -w
+fi
+
 date >>/home/pi/Pies/DeviceInfo/`cat /home/pi/deviceid`.info
 cat /home/pi/deviceid >>/home/pi/Pies/DeviceInfo/`cat /home/pi/deviceid`.info
 ifconfig wlan0 >>/home/pi/Pies/DeviceInfo/`cat /home/pi/deviceid`.info
