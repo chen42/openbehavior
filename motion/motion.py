247575f5c679e1436e7359fd3b91a2671526e394
Hao Chen <inbox@haochen.name>
2016-06-10 16:58:24 -0500

    minor change in data file format, switch on/off mechanism

--- motion/motion.py
+++ motion/motion.py
@@ -15,8 +15,8 @@ gpio.setup(onPin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 gpio.setup(motionLed, gpio.OUT)        
 
 while(True):
-	if gpio.input(onPin):
-                sessionLength=3600
+	if gpio.input(onPin) == 0 :
+                sessionLength=7200
 		idfile=open("/home/pi/deviceid")
 		boxid=idfile.read()
                 boxid=boxid.strip()
@@ -28,27 +28,27 @@ while(True):
 			f.write("#Session started on " + time.strftime("%Y-%m-%d\t%H:%M:%S\t", time.localtime())+"\n")
 			f.write("date\tboxid\tseconds\n")
 			f.close()
-			
+		#print time.strftime("%Y-%m-%d\t%H:%M:%S")
 		while time.time()-start < sessionLength:
 			if gpio.input(pirPin):
-				print time.strftime("%Y-%m-%d\t%H:%M:%S")
                                 cnt=cnt+1
 				with open(motionDataFile,"a") as f:
-					lapsed=time.time()-start
-					f.write(time.strftime("%Y-%m-%d\t", time.localtime()) + boxid +"\t"+ str(lapsed) +"\n")
-					f.close()
+                                    lapsed=time.time()-start
+                                    f.write(time.strftime("%Y-%m-%d\t", time.localtime()) + boxid +"\t"+ str(lapsed) +"\n")
+				    f.close()
 				gpio.output(motionLed, True)
 				time.sleep(0.1)
 				gpio.output(motionLed, False)
 				time.sleep(0.1)
-                                if not gpio.input(onPin):
-                                    sessionLength=0
-		with open(motionDataFile, "a") as f:
-			f.write("Total\t"+str(cnt) + "\n")
-			f.write("#Session ended at " + time.strftime("%H:%M:%S", time.localtime()) + "\n")
-			f.close()
+                        if gpio.input(onPin) == 1:
+                            with open(motionDataFile,"a") as f:
+                                f.write("#Session ended\t" + time.strftime("%H:%M:%S", time.localtime()) + "\n") 
+                                f.write(boxid + "\t" + str(cnt)+"\n")
+                                f.close()
+                            sessionLength=0
+                            os.system("/home/pi/openbehavior/wifi-network/rsync.sh &")
 	else:
 		time.sleep(1)
-                print "idle\n"
-
+		gpio.output(motionLed, True)
+                #print "pin 16 has no signal\n"
 
