e42dc800dbe03bb0181b32bd41acfac71a30ed3f
Matthew Longley <randomticktock@gmail.com>
2016-07-08 15:51:03 -0500

    resized root window

--- touchscreen/touchscreen.py
+++ touchscreen/touchscreen.py
@@ -20,7 +20,7 @@ button1.pack(side=LEFT, padx=2, pady=2)
 button2=Button(frame1, compound=TOP, width=600, height=600, image=circle, command=Incorrect)
 button2.pack(side=RIGHT, padx=2, pady=2)
 
-
+root.geometry('{}x{}'.format(1366,768))
 
 root.mainloop()
 root.destroy()
