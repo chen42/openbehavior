1ec1e3907ba72469491d6c0c572fc6f4a4955efb
Matthew Longley <randomticktock@gmail.com>
2016-07-08 15:52:59 -0500

    resized buttons

--- touchscreen/touchscreen.py
+++ touchscreen/touchscreen.py
@@ -14,10 +14,10 @@ circle=PhotoImage(file='circle.png')
 frame1=Frame(root)
 frame1.pack(side=TOP, fill=X)
 star=PhotoImage(file='star.png')
-button1=Button(frame1, compound=TOP, width=600, height=600, image=star, command=Correct)
+button1=Button(frame1, compound=TOP, width=683, height=384, image=star, command=Correct)
 button1.pack(side=LEFT, padx=2, pady=2)
 
-button2=Button(frame1, compound=TOP, width=600, height=600, image=circle, command=Incorrect)
+button2=Button(frame1, compound=TOP, width=683, height=384, image=circle, command=Incorrect)
 button2.pack(side=RIGHT, padx=2, pady=2)
 
 root.geometry('{}x{}'.format(1366,768))
