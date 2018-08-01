e6ee9252c01d1fe925b196a2515da14de977efdd
Hao Chen <inbox@haochen.name>
2018-08-01 10:35:11 -0500

    remove buzzer, calibrate time

--- tailwithdrawal/tailwithdrawal.py
+++ tailwithdrawal/tailwithdrawal.py
@@ -66,9 +66,9 @@ def rfid():
 def setupGPIO():
 	GPIO.setmode(GPIO.BOARD)	# Numbers GPIOs by physical location
 	GPIO.setup(Tail, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(Buzzer, GPIO.OUT)	# Set pins' mode is output
-	global Buzz			# Assign a global variable to replace GPIO.PWM 
-	Buzz = GPIO.PWM(Buzzer, 1)	# 1 is initial frequency.
+#	GPIO.setup(Buzzer, GPIO.OUT)	# Set pins' mode is output
+#	global Buzz			# Assign a global variable to replace GPIO.PWM 
+#	Buzz = GPIO.PWM(Buzzer, 1)	# 1 is initial frequency.
 
 
 
@@ -81,23 +81,14 @@ def read_temp_raw():
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.05)
+        time.sleep(0.01)
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         return temp_c
-def buzz():
-	Buzz.start(50)			# Start Buzzer pin with 50% duty ration
-#	Buzz.ChangeFrequency(131)	# Change the frequency along the song note
-#	time.sleep(.5)			# delay a note for beat * 0.5s
-	Buzz.ChangeFrequency(350)	# Change the frequency along the song note
-	time.sleep(.5)			# delay a note for beat * 0.5s
-	Buzz.ChangeFrequency(1)	# Change the frequency along the song note
-	Buzz.stop(50)			# Start Buzzer pin with 50% duty ration
 
-Buzzer = 11
 Tail=12
 setupGPIO()
 
@@ -110,23 +101,21 @@ print ("data are saved in " + datafile+"\n")
 ratid=rfid()
 
 while True:
-	time.sleep(0.5)
+	time.sleep(0.01)
 	tail_out= GPIO.input(Tail)
 	if (tail_out==True):
 		temp1=read_temp()
 		print ("current temp:\t" + str(temp1))
 		if (temp1>temphi):
-			print ("temperature too high\n")
-			buzz()
+			print ("\t\t\tTemperature too high\n")
 		elif (temp1<templo):
-			print ("temperature too low\n")
-			buzz()
+			print ("\t\t\tTemperature too low\n")
 	else: 
 		sTime=time.time()
 		print "start timer\n"
 		while (tail_out==False):
 			tail_out= GPIO.input(Tail)
-		elapsed=time.time()-sTime		
+		elapsed=time.time()-sTime +0.4 # calibrated at 2s and 10s  found this consistent system error		
 		elapsed=round(elapsed, 3)
 		temp2=read_temp()
 		temp=round((temp1+temp2)/2, 3)
