883457b0821fea1a325c62d0ae6ace8211b3bfef
Hao Chen <inbox@haochen.name>
2019-04-30 14:37:03 -0500

    retain up to five lines

--- RFID_eInk_Reader/rfid_reader_eink.py
+++ RFID_eInk_Reader/rfid_reader_eink.py
@@ -4,29 +4,32 @@
 import epd2in7
 import time
 from PIL import Image,ImageDraw,ImageFont
-import traceback
+import os 
+
 
 epd = epd2in7.EPD()
 epd.init()
 epd.Clear(0xFF)
-    
-    
+
 def show(text):
     Himage = Image.new('1', (epd2in7.EPD_HEIGHT, epd2in7.EPD_WIDTH), 255)  # 255: clear the frame
-    # Horizontal
-    print("Drawing")
     draw = ImageDraw.Draw(Himage)
     font32 = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf',32)
     draw.text((10, 0), text, font = font32, fill = 0)
     epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
-        
 
 show("Ready to scan")
+line=""
+cnt=0
 while True:
-    rfid=raw_input("Waiting for RFID ")
+    if (cnt == 5 ):
+        cnt=0
+        line=""
+    cnt=cnt+1
+    rfid=input("Waiting for RFID")
     rfid=hex(int(rfid)).upper()
-    show(rfid)
-    time.sleep(2)
+    line=line +  rfid[2:] + "\n"
+    show(line)
+    time.sleep(.5)
 
 epd.sleep()
