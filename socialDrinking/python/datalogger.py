db9f92b4a974d43b588e63ba8188f3849a4e3be8
Hao Chen <inbox@haochen.name>
2019-09-19 06:00:56 -0500

    re-org data logger

--- socialDrinking/python/datalogger.py
+++ socialDrinking/python/datalogger.py
@@ -1,52 +1,39 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-# BEGIN IMPORT PRELUDE
 import time
 import string
-# END IMPORT PRELUDE
 
-# BEGIN CONSTANT DEFINITIONS
-DEVID_FILEPATH = '/home/pi/deviceid'
-RATID_FILEPATH = '/home/pi/ratid'
-TDATA_FILEPRFX = '/home/pi/Pies/ETOH/ETOH_'
-SESSIONID='/home/pi/sessionid'
-# END CONSTANT DEFINITIONS
+ROOT='/home/pi'
+ROOT="."
+DEVID_FILE = ROOT+'/deviceid'
+RATID_FILE = ROOT+'/ratids'
+SESSIONID_FILE = ROOT+'/sessionid'
+DATAFILE_PRFX = ROOT+'/Pies/Oxycodone/Oxy_'
 
 class LickLogger:
-	def __init__(self):
-		# read box id
-		self.datatype="lick"
-	def createDataFile(self, RatID, schedule):
-		devidfile = open(DEVID_FILEPATH)
-		self.devid = str((devidfile.read()).strip())
-		devidfile.close()
+    def __init__(self):
+        with open (DEVID_FILE) as devID:
+            self.devID = str((devID.read()).strip())
+            devID.close()
+        with open (SESSIONID_FILE) as sessID:
+            self.sessID=str(sessID.read().strip())
+            sessID.close()
+        with open (RATID_FILE) as ratID:
+            self.ratID = str((ratID.read()).strip())
+            ratID.close()
+        self.startTime=time.strftime("%Y-%m-%d\t%H:%M:%S", time.localtime())
 
-		# read rat id
-		self.ratid = RatID 
+    def createDataFile(self, schedule):
+        self.datafile = DATAFILE_PRFX + str(self.devID) + '_S' + self.sessID +  "_" +  str(self.ratID) + '.csv'
+        print (self.datafile)
+        # open data file
+        with open(self.datafile,"a") as f:
+            f.write("RatID\tdate\tstart_time\tboxid\tEventType\t"+schedule+"\tseconds\n")
+            f.close()
 
-                # read sessin id
-                sessionid=open(SESSIONID)
-                self.sessid=str(sessionid.read().strip())
-                #print ("sessionid ", self.sessid, "\n") 
+    def logEvent(self, rat, eventType, timeLapsed, ratio=0):
+        # Create output string
+        outputstr = rat + "\t" + time.strftime("%Y-%m-%d\t%H:%M:%S", time.localtime()) + "\t" + self.devID + "_S" + self.sessID+ "\t" + eventType + "\t" + str(ratio) + "\t"+ str(timeLapsed) + "\n"
+        with open (self.datafile, "a") as datafile:
+            datafile.write(outputstr)
 
-		# get start time
-		startTime=time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-
-		# construct file name
-                self.datafile = TDATA_FILEPRFX + self.datatype + "_" +  str(self.devid)[5:] + '_S' + self.sessid +  "_" +  str(self.ratid) + '.csv'
-
-		# open data file
-		with open(self.datafile,"a") as f:
-			f.write("RatID\tdate\tboxid\tEventType\t"+schedule+"\tseconds\n")
-			f.close()
-	def logEvent(self, EventType, timelapsed, ratio=0):
-		# Create output string
-		outputstr = self.ratid + "\t" + time.strftime("%Y-%m-%d", time.localtime()) + "\t" + self.devid + "_S" + self.sessid+ "\t" + EventType + "\t" + str(ratio) + "\t"+ str(timelapsed) + "\n"
-		# Append to file
-		with open (self.datafile, "a") as datafile:
-			datafile.write(outputstr)
-
-
-class MotionLogger (LickLogger):
-	def __init__ (self):
-		self.datatype="motion"
