e52692f9d8670b6976983fda19aff98a1f717794
Jie Ni <nijie321@outlook.com>
2020-10-15 10:28:40 -0500

    use last 8 char for initial RFID

--- socialDrinking/python/main.py
+++ socialDrinking/python/main.py
@@ -101,8 +101,8 @@ h=str(int(sessionLength/3600))
 print("Run " + schedule + str(ratio) + "for "  + h + "h\n")
 
 time.sleep(3)
-rat1 = input("please scan rat1\n")
-rat2 = input("please scan rat2\n")
+rat1 = input("please scan rat1\n")[-8:]
+rat2 = input("please scan rat2\n")[-8:]
 
 while(rat1[-8:] == rat2[-8:]):
     rat2 = input("The IDs of rat1 and rat2 are identical, please scan rat2 again\n")
