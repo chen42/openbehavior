7fe104ab0190a409da77f40952ac1b7cd80c6bba
Jie Ni <nijie321@outlook.com>
2020-12-16 19:54:10 -0600

    add script to check empty program files at start up

---
+++ socialDrinking/utility_script/check_empty_program.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+program_file_dir="/home/pi/openbehavior/socialDrinking/python"
+
+num_empty_files=`ls -l $program_file_dir/*.py | awk '{if($7==4) print($9)}' | wc -l`
+
+if [ $num_empty_files -gt 0 ]
+then
+  echo "There are $num_empty_files program files. Please check"
+  exit 1
+fi
+
+
+exit 0
+
