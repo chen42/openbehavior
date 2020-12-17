da753c290ae3c5f0a6f5ef32c14d75d2d6412e3f
Jie Ni <nijie321@outlook.com>
2020-12-16 22:55:14 -0600

    Update check_empty_program.sh

--- socialDrinking/utility_script/check_empty_program.sh
+++ socialDrinking/utility_script/check_empty_program.sh
@@ -2,7 +2,7 @@
 
 program_file_dir="/home/pi/openbehavior/socialDrinking/python"
 
-num_empty_files=`ls -l $program_file_dir/*.py | awk '{if($7==4) print($9)}' | wc -l`
+num_empty_files=`ls -l $program_file_dir/*.py | awk '{if($7==0) print($9)}' | wc -l`
 
 if [ $num_empty_files -gt 0 ]
 then
