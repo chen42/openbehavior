10601f3ebdf4a4d749751684b861610e48ba5fe3
Jie Ni <nijie321@outlook.com>
2020-12-17 07:49:43 -0600

    remove the corrupted project and reclone if any of the program file is not found

--- socialDrinking/utility_script/check_empty_program.sh
+++ socialDrinking/utility_script/check_empty_program.sh
@@ -4,12 +4,23 @@ program_file_dir="/home/pi/openbehavior/socialDrinking/python"
 
 num_empty_files=`ls -l $program_file_dir/*.py | awk '{if($7==0) print($9)}' | wc -l`
 
-if [ $num_empty_files -gt 0 ]
-then
-  echo "There are $num_empty_files program files. Please check"
-  exit 1
-fi
+cd /home/pi
+
+redownload_code=`rm -rf ./openbehavior && git clone https://github.com/chen42/openbehavior.git`
+
+while kill -0 $redownload_code ; do
+  echo "Program is being downloded. Please Wait....."
+  sleep 1
+done
+
+echo "Finished...."
 
 
-exit 0
+# if [ $num_empty_files -gt 0 ]
+# then
+#   echo "There are $num_empty_files program files. Please check"
+#   exit 1
+# fi
 
+
+# exit 0
